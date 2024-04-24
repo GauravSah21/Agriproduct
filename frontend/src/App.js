00a802108c8343fa301960df4568502d106b1dc1
Abhishek Bhardwaj <abhiktd15@gmail.com>
2024-04-24 15:24:00 +0530

    Removed Cart option and order history for Admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -93,22 +93,24 @@ function App() {
               <Navbar.Collapse id="basic-navbar-nav">
                 <SearchBox />
                 <Nav className="me-auto  w-100  justify-content-end">
-                  <Link to="/cart" className="nav-link">
+                  {!userInfo.isAdmin && <Link to="/cart" className="nav-link">
                     Cart
                     {cart.cartItems.length > 0 && (
                       <Badge pill bg="danger">
                         {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                       </Badge>
                     )}
-                  </Link>
+                  </Link>}
                   {userInfo ? (
                     <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
                       <LinkContainer to="/profile">
                         <NavDropdown.Item>User Profile</NavDropdown.Item>
                       </LinkContainer>
-                      <LinkContainer to="/orderhistory">
-                        <NavDropdown.Item>Order History</NavDropdown.Item>
-                      </LinkContainer>
+                      {!userInfo.isAdmin && 
+                        <LinkContainer to="/orderhistory">
+                          <NavDropdown.Item>Order History</NavDropdown.Item>
+                        </LinkContainer>
+                      }
                       <NavDropdown.Divider />
                       <Link
                         className="dropdown-item"
