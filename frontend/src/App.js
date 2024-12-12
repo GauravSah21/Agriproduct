d739b07aa464e44f7b75e395136baa345051ef96
Gauravkrsah <gauravkumarsah.078@kathford.edu.np>
2024-12-12 22:00:44 +0545

    searchbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -87,7 +87,7 @@ function App() {
               </Button>
 
               <LinkContainer to="/">
-                <Navbar.Brand>AgriProduct</Navbar.Brand>
+                <Navbar.Brand>Cropsay</Navbar.Brand>
               </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
